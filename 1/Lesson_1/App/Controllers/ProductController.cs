4cbc6f028c3d4efd79bbc716cef444851ed80e96
GSY <gle722@yandex.ru>
2024-03-20 22:12:34 +0300

    HW_1

--- 1/Lesson_1/App/Controllers/ProductController.cs
+++ 1/Lesson_1/App/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using App.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace App.Controllers
 {
@@ -59,5 +61,146 @@ namespace App.Controllers
                 return StatusCode(500);
             }
         }
+
+        [HttpPost(template:"postCategory")]
+        public IActionResult PostCategory([FromQuery] string name, string description)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (!context.Category.Any(x => x.Name.ToLower().Equals(name.ToLower())))
+                    {
+                        context.Add(new Category()
+                        {
+                            Name = name,
+                            Description = description,                            
+                        });
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost(template: "postStorage")]
+        public IActionResult PostStorage([FromQuery] string name, string description, int productId, int amount)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (!context.Storages.Any(x => x.Name.ToLower().Equals(name.ToLower())))
+                    {
+                        context.Add(new Storage()
+                        {
+                            Name = name,
+                            Description = description,
+                            ProductId = productId,
+                            Amount = amount
+                        });
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPatch(template:"changePrice")]
+        public IActionResult ChangePrice([FromQuery] int productId, int price)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Id == productId))
+                    {
+                        var product = context.Products.Find(productId);
+                        if (product != null)
+                        {
+                            product.Cost = price;
+                        }
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete(template:"deleteProduct")]
+        public IActionResult DeleteProduct([FromQuery] int productId)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Id == productId))
+                    {
+                        var product = context.Products.Find(productId);
+                        context.Products.Remove(product);
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpDelete(template: "deleteCategory")]
+        public IActionResult DeleteCategory([FromQuery] int categoryId)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Category.Any(x => x.Id == categoryId))
+                    {
+                        var category = context.Category.Find(categoryId);
+                        context.Category.Remove(category);
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
     }
 }
